f88af2b9a0090d80ba863f720f1dbfb15839196b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-08 00:43:05 +0530

    form

--- crudreact/src/components/create.component.js
+++ crudreact/src/components/create.component.js
@@ -3,8 +3,26 @@ import React,{Component} from 'react';
 export default class Create extends Component{
     render(){
         return(
-            <div>
-                Welcome to create Component
+            <div style={{marginTop:10}}>
+                <h3> 
+                    Add New Business
+                </h3>
+                <form>
+                    <div className="form-group">
+                        <label>Add Person Name :</label>
+                        <input type="text" className="form-control"/>
+                     </div>
+
+                     <div className="form-group">
+                        <label>Add Person Name :</label>
+                        <input type="text" className="form-control"/>
+                     </div>
+
+                     <div className="form-group">
+                        <label>Add Person Name :</label>
+                        <input type="text" className="form-control"/>
+                     </div>
+                </form>
             </div>
         );
     }
