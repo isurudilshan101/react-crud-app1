a0aec12c6a969843a49ef821b6cc95e6facbff17
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-08 00:33:08 +0530

    nav

--- crudreact/src/App.js
+++ crudreact/src/App.js
@@ -9,11 +9,38 @@ import Index from './components/index.component';
 class App extends Component{
 
   render(){
-    
+
     return(
+      <Router>
       <div className="container">
-        <h2>React CRUD by Isuru</h2>
-      </div>
+          <nav className="navbar navbar-expand-lg nabar-light bg-light">
+            <Link to={'/'} className="nabar-brand">REACT CRUD Example By Isuru</Link>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav mr-auto">
+                      <li className="nav-item">
+                          <Link to={'/'} className="nav-link">Home</Link>
+                      </li>
+
+                      <li className="nav-item">
+                          <Link to={'/create'} className="nav-link">Create</Link>
+                      </li>
+
+                      <li className="nav-item">
+                          <Link to={'/index'} className="nav-link">Index</Link>
+                      </li>
+                  </ul>
+              </div>
+          </nav>  <br/>
+          <h2>Welcome to REACT CRUD</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={Create}/>
+              <Route exact path='/edit/:id' component={Edit}/>
+              <Route exact path='/index' component={Index}/>
+
+          </Switch>
+        </div>
+  </Router>
+
     );
   }
 }
--- crudreact/src/components/create.component.js
+++ crudreact/src/components/create.component.js
@@ -1,26 +1,11 @@
 import React,{Component} from 'react';
 
-export default class Ceate extends Component{
+export default class Create extends Component{
     render(){
         return(
-            <Router>
-                <div className="container">
-                    <nav className="navbar navbar-expand-lg nabar-light bg-light">
-                      <Link to='/' className="nabar-brand">REACT CRUD Example By Isuru</Link>
-                        <div className="collapse navbar-collapse id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item">
-                                    <Link to={'/'} className="nav-link">Home</Link>
-                                </li>
-
-                                <li className="nav-item">
-                                    <Link to={'/'} className="nav-link">Home</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    
-                </div>
-            </Router>
-        )
+            <div>
+                Welcome to create Component
+            </div>
+        );
     }
 }
