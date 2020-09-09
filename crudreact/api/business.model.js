2bb56b65efbb6a96919fdfc1252382f25537f064
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-10 02:18:58 +0530

    Schema

---
+++ crudreact/api/business.model.js
@@ -0,0 +1,15 @@
+const ongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+let Business=new Schema({
+    person_name:{
+        type:String
+    },
+
+    business_name:{
+        type:String
+    },
+    person_nic_number:{
+        type:Number
+    }
+})
