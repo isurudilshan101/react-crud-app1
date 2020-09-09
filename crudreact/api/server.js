6876d5e15280407ee31e4206a8b2c197dfe3b3fd
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-10 02:11:38 +0530

    mongodb

--- crudreact/api/server.js
+++ crudreact/api/server.js
@@ -4,6 +4,20 @@ const bodyParser=require('body-parser');
 const PORT=4000;
 const cors=require('cors');
 
+const mongoose=require('mongoose');
+const config=require('./DB');
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect(config.DB,{useNewUrlParser:true}).then(
+    ()=>{
+        console.log('Database is Connected')
+    },
+    err=>{
+        console.log('Cannot connect to the database' + err)
+    }
+);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json);
