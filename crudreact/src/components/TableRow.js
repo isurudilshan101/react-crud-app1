87a30b765512c0572bee1998dfd1ea0e50ee76cc
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-11 18:13:00 +0530

    TableRow

--- crudreact/src/components/TableRow.js
+++ crudreact/src/components/TableRow.js
@@ -4,9 +4,28 @@ class TableRow extends Component{
 
     render(){
         return(
-            <div>
+           
+            <tr>
+                <td>
+                    {this.props.obj.person_name}
+                </td>
+                <td>
+                    {this.props.obj.business_name}
+                </td>
 
-            </div>
+                <td>
+                    {this.props.obj.business_nic_number}
+                </td>
+
+                <td>
+                    <button className="btn btn-primary">Edit</button>
+                </td>
+
+                <td>
+                    <button className="btn btn-primary">Delete</button>
+                </td>
+            </tr>
+
         );
     }
 }
